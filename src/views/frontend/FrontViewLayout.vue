27b2c2518dad98aa5a897e5a0d5596f586bc8219
KumaShow <kumadth@gmail.com>
2023-03-06 15:57:13 +0800

    更新部分元件名稱
    更新 BreadCrumb、ToolBox 名稱

--- src/views/frontend/FrontViewLayout.vue
+++ src/views/frontend/FrontViewLayout.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="front-layout">
-    <FrontNavBar />
-    <TheBreadcrumb v-if="this.$route.path !== '/'" />
-    <RouterView class="min-height" />
-    <TheToolBox class="the-tool-box" />
+    <FrontNavBar :is-mobile="isMobile" />
+    <BreadCrumb v-show="this.$route.path !== '/'" />
+    <RouterView />
+    <ToolBox class="the-tool-box" />
     <FrontFooter />
   </div>
 </template>
 
 <script>
 import FrontNavBar from "@/components/frontend/FrontNavBar.vue";
-import TheToolBox from "@/components/frontend/TheToolBox.vue";
+import ToolBox from "@/components/frontend/ToolBox.vue";
 import FrontFooter from "@/components/frontend/FrontFooter.vue";
-import TheBreadcrumb from "@/components/frontend/TheBreadcrumb.vue";
+import BreadCrumb from "@/components/frontend/BreadCrumb.vue";
 
 export default {
   components: {
     FrontNavBar,
-    TheToolBox,
+    ToolBox,
     FrontFooter,
-    TheBreadcrumb,
+    BreadCrumb,
+  },
+  data() {
+    return {
+      isMobile: false,
+    };
+  },
+  mounted() {
+    // 判斷使用者設備寬度(若直接在 PC 拖拉視窗，需重整抓寬度判斷)
+    this.isMobile = window.matchMedia("(max-width: 768px)").matches;
   },
 };
 </script>
@@ -33,7 +42,7 @@ export default {
   right: 50px;
   bottom: 80px;
 }
-.min-height {
+.main-height {
   min-height: calc(100vh - 64px - 48px - 67px);
 }
 </style>
