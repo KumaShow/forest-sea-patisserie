7c566eb9dc0e50c28a7dd893daadfbfac80c91b9
KumaShow <kumadth@gmail.com>
2023-03-06 15:43:37 +0800

    更新首頁樣式

--- src/views/frontend/HomeView.vue
+++ src/views/frontend/HomeView.vue
@@ -17,28 +17,39 @@
         >
       </div>
       <div class="section-banner-more text-center">
-        <img src="@/assets/images/readMoreArrow.png" alt="往下閱讀更多" />
+        <img
+          src="@/assets/images/readMoreArrow.png"
+          alt="往下閱讀更多"
+          ref="readMore"
+        />
         <p class="text-white mt-6">閱讀更多</p>
       </div>
     </section>
     <!-- 最新消息 -->
-    <!-- TODO: 圖片樣式修改 -->
-    <section class="section-news bg-primary-dark py-16">
+    <section class="section-news bg-primary-dark py-11 py-lg-16">
       <div class="container">
         <div class="news-content row">
-          <div class="col-6">
-            <img
-              class="w-100"
-              src="@/assets/images/news.png"
-              alt="最新消息圖片"
-            />
+          <!-- 圖片 -->
+          <div class="col-lg-6 mb-8 mb-md-0">
+            <div class="text-center mb-11 d-lg-none">
+              <h2 class="font-serif text-white">最新消息</h2>
+              <img src="@/assets/images/header_line.svg" alt="文章圖片" />
+            </div>
+            <div class="p-6 bg-white overflow-hidden news-img">
+              <img
+                class="w-100 object-cover"
+                src="@/assets/images/news.png"
+                alt="最新消息圖片"
+              />
+            </div>
           </div>
-          <div class="col-6 text-white">
-            <div class="text-center mb-10">
+          <!-- 文章列表 -->
+          <div class="col-lg-6 text-white">
+            <div class="text-center d-none d-lg-block mb-10 my-md-8 mb-lg-10">
               <h2 class="font-serif">最新消息</h2>
               <img src="@/assets/images/header_line.svg" alt="" />
             </div>
-            <ul class="px-9">
+            <ul class="px-md-9">
               <li class="py-9 border-white border-bottom border-opacity-50">
                 <div class="d-flex align-items-center mb-2">
                   <span class="fs-8 py-1 px-2 me-6 bg-secondary rounded-1"
@@ -46,10 +57,11 @@
                   >
                   <p class="text-white text-opacity-80">12月 5日 2022</p>
                 </div>
-                <h3 class="text-truncate fs-4">
+                <h3 class="text-truncate fs-5 fs-md-4">
                   <a href="#!">2022聖誕禮盒預約活動開始啦！</a>
                 </h3>
               </li>
+
               <li class="py-9 border-white border-bottom border-opacity-50">
                 <div class="d-flex align-items-center mb-2">
                   <span class="fs-8 py-1 px-2 me-6 bg-secondary rounded-1"
@@ -89,9 +101,9 @@
       </div>
     </section>
     <!-- 熱門商品 -->
-    <section class="section-hot py-16 bg-secondary-100">
+    <section class="bg-mask-texture py-11 py-lg-16 bg-secondary-100">
       <div class="container">
-        <div class="text-center mb-15">
+        <div class="text-center mb-11 mb-lg-15">
           <h2 class="font-serif">熱門商品</h2>
           <img src="@/assets/images/header_line.svg" alt="" />
         </div>
@@ -99,47 +111,68 @@
       </div>
     </section>
     <!-- 實體店面 -->
-    <section class="section-store my-16">
+    <section class="section-store py-11 py-lg-16">
       <div class="container">
-        <div class="text-center mb-15">
+        <div class="text-center mb-11 mb-lg-15">
           <h2 class="text-center font-serif">實體店面</h2>
           <img src="@/assets/images/header_line.svg" alt="" />
         </div>
-        <div class="row align-items-center">
+        <div class="row align-items-center flex-row-reverse">
+          <!-- 地圖 -->
+          <div class="col-lg-6 mb-11 mb-lg-0">
+            <div class="p-4 p-mb-6 box-shadow-1">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d918.220245848611!2d120.1999951301751!3d22.99140362892596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e767cad121e19%3A0x299a83f8a02bcdca!2zNzAw5Y-w54Gj5Y-w5Y2X5biC5Lit6KW_5Y2A5Y-L5oSb6KGXMTA16Jmf!5e0!3m2!1szh-TW!2ssg!4v1677846674578!5m2!1szh-TW!2ssg"
+                style="border: 0"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+                class="w-100 store-map"
+              ></iframe>
+            </div>
+          </div>
+          <!-- 聯絡資訊 -->
           <div class="col-lg-6 lh-sm">
-            <div class="ms-11">
-              <h3 class="mb-2 text-neutral-900 fs-2">台南本店</h3>
-              <p class="text-neutral-700 fs-5 mb-9">台南市中西區友愛街105號</p>
+            <div class="ms-lg-11 text-center text-lg-start">
+              <h3 class="mb-2 text-neutral-900 fs-4 fs-lg-2">台南本店</h3>
+              <a
+                href="https://www.google.com/maps/search/?api=1&query=台南市中西區友愛街105號"
+                target="_blank"
+                class="text-neutral-700 fs-5 mb-9 d-block"
+                >台南市中西區友愛街105號</a
+              >
               <!-- 本月公休 -->
-              <div class="d-flex align-items-center mb-9">
-                <h4 class="me-6 fs-7 text-neutral-900">本月公休：</h4>
-                <ul class="d-flex">
+              <div class="d-lg-flex align-items-center mb-9">
+                <h4 class="me-lg-6 mb-6 mb-lg-0 fs-7 text-neutral-900">
+                  本月公休：
+                </h4>
+                <ul class="d-flex justify-content-center">
                   <li
-                    class="me-6 border border-secondary d-flex justify-content-center align-items-center rounded-1 text-secondary"
+                    class="me-6 border border-secondary d-flex justify-content-center align-items-center rounded-1 text-secondary box-shadow-3"
                     style="height: 32px; width: 32px"
                   >
                     6
                   </li>
                   <li
-                    class="me-6 border border-secondary d-flex justify-content-center align-items-center rounded-1 text-secondary"
+                    class="me-6 border border-secondary d-flex justify-content-center align-items-center rounded-1 text-secondary box-shadow-3"
                     style="height: 32px; width: 32px"
                   >
                     13
                   </li>
                   <li
-                    class="me-6 border border-secondary d-flex justify-content-center align-items-center rounded-1 text-secondary"
+                    class="me-6 border border-secondary d-flex justify-content-center align-items-center rounded-1 text-secondary box-shadow-3"
                     style="height: 32px; width: 32px"
                   >
                     19
                   </li>
                   <li
-                    class="me-6 border border-secondary d-flex justify-content-center align-items-center rounded-1 text-secondary"
+                    class="me-6 border border-secondary d-flex justify-content-center align-items-center rounded-1 text-secondary box-shadow-3"
                     style="height: 32px; width: 32px"
                   >
                     20
                   </li>
                   <li
-                    class="me-6 border border-secondary d-flex justify-content-center align-items-center rounded-1 text-secondary"
+                    class="me-6 border border-secondary d-flex justify-content-center align-items-center rounded-1 text-secondary box-shadow-3"
                     style="height: 32px; width: 32px"
                   >
                     27
@@ -147,113 +180,54 @@
                 </ul>
               </div>
               <!-- 連絡電話 -->
-              <div class="d-flex align-items-center text-neutral-900 mb-9">
-                <h4 class="me-6 fs-7">連絡電話：</h4>
+              <div class="d-lg-flex align-items-center text-neutral-900 mb-9">
+                <h4 class="me-lg-6 mb-6 mb-lg-0 fs-7">連絡電話：</h4>
                 <a href="tel:+886-62111891" class="link-neutral-900">
                   06 - 2111891
                 </a>
               </div>
               <!-- 營業時間： -->
-              <div class="d-flex align-items-center text-neutral-900">
-                <h4 class="me-6 fs-7">營業時間：</h4>
+              <div class="d-lg-flex align-items-center text-neutral-900">
+                <h4 class="me-lg-6 mb-6 fs-7">營業時間：</h4>
                 <p>週一至週日12:00 - 19:00 (週二除外)</p>
               </div>
-            </div>
-          </div>
-          <div class="col-lg-6">
-            <!-- TODO: 串接 Map -->
-            <img src="@/assets/images/map.png" alt="" class="w-100" />
-          </div>
-        </div>
-      </div>
-    </section>
-    <!-- 訂閱消息 -->
-    <section class="section-subscribe bg-primary-dark py-16">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6">
-            <h2 class="text-white mb-1">訂閱優惠訊息</h2>
-            <p class="text-white opacity-80 mb-8">
-              立即訂閱，取得最新折扣優惠碼
-            </p>
-            <div>
-              <input
-                type="email"
-                class="rounded-1 p-4 w-75 me-2"
-                placeholder="請輸入信箱"
-              />
-              <button
-                type="button"
-                class="btn btn-secondary py-4 px-10 text-white fw-semibold"
-              >
-                訂閱
-              </button>
-            </div>
-          </div>
-          <div class="col-lg-6 text-end">
-            <div class="">
-              <img
-                src="@/assets/images/logo.svg"
-                style="width: 120px; height: auto"
-                class="mb-8"
-                alt="Logo"
-              />
-              <ul class="d-flex justify-content-end mb-8">
-                <li>
-                  <a
-                    href="https://www.facebook.com/ForestSeaPatisserie"
-                    class="me-11"
-                    >FB</a
-                  >
-                </li>
-                <li>
-                  <a
-                    href="https://page.line.me/?accountId=033evgma"
-                    class="me-11"
-                    >Line</a
-                  >
-                </li>
-                <li>
-                  <a
-                    href="https://www.instagram.com/patisserie_forest_sea/?igshid=ebj7xy2g2aa1"
-                    >IG</a
-                  >
-                </li>
-              </ul>
-              <ul class="d-flex justify-content-end">
-                <li class="me-9 me-lg-8">
-                  <RouterLink to="products">商品一覽</RouterLink>
-                </li>
-                <li class="me-9 me-lg-8">
-                  <RouterLink to="news">最新消息</RouterLink>
-                </li>
-                <li class="me-9 me-lg-8">
-                  <RouterLink to="about">關於我們</RouterLink>
-                </li>
-                <li><RouterLink to="faq">常見問題</RouterLink></li>
-              </ul>
             </div>
           </div>
         </div>
       </div>
     </section>
   </main>
-  <!-- <TheToolBox /> -->
 </template>
 
 <script>
 import ProductsCarousel from "@/components/frontend/ProductsCarousel.vue";
-// import TheToolBox from "@/components/TheToolBox.vue";
+import gsap from "gsap";
 
 export default {
   components: {
     ProductsCarousel,
-    // TheToolBox,
+  },
+  props: ["isMobile"],
+  data() {
+    return {};
+  },
+  mounted() {
+    const readMoreImg = this.$refs.readMore;
+
+    gsap.to(readMoreImg, {
+      y: 15,
+      duration: 0.5,
+      ease: "Power1.inOut",
+      repeat: -1,
+      yoyo: true,
+    });
   },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/stylesheets/helpers/mixin.scss";
+
 .section-banner {
   min-height: 85vh;
   background-image: url("@/assets/images/landing.png");
@@ -272,14 +246,23 @@ export default {
   }
 }
 
-.section-hot {
-  background-image: url("@/assets/images/Noise\ &\ Texture.png");
+.section-news {
+  .news-img img {
+    height: 420px;
+
+    @include respond-min(lg) {
+      height: 720px;
+    }
+  }
 }
 
-.section-subscribe {
-  background-image: url("@/assets/images/subscribe.png");
-  background-size: cover;
-  background-position: center;
-  opacity: 0.65;
+.section-store {
+  .store-map {
+    height: 188px;
+
+    @include respond-min(md) {
+      height: 340px;
+    }
+  }
 }
 </style>
