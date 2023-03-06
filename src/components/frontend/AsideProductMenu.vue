b68516d9168f8c5beb06c52ecd91be276bbffd15
KumaShow <kumadth@gmail.com>
2023-03-06 15:48:38 +0800

    更新側邊產品選單元件

--- src/components/frontend/AsideProductMenu.vue
+++ src/components/frontend/AsideProductMenu.vue
@@ -1,28 +1,88 @@
 <!-- 
   TODO:
-    - 連結依據類別變動
-    - 樣式調整
-    - sticky
+    - 連結依據類別變動 v
+    - 樣式調整 v
+  BUG:
+    - 頁面跳轉問題: 跳轉頁面不會回到全部商品，會保留在上一個類別
 -->
 <template>
-  <aside>
-    <h3 class="fs-5 mb-6">商品類別</h3>
-    <ul>
-      <li class="mb-6">
-        <a href="" class="link-dark">- 全部商品</a>
-      </li>
-      <li class="mb-6"><a href="" class="link-dark">- 可麗露</a></li>
-      <li class="mb-6"><a href="" class="link-dark">- 瑪德蓮</a></li>
-      <li class="mb-6"><a href="" class="link-dark">- 磅蛋糕(長條)</a></li>
-      <li class="mb-6"><a href="" class="link-dark">- 磅蛋糕(整模)</a></li>
-      <li class="mb-6"><a href="" class="link-dark">- 限定商品</a></li>
-      <li class="mb-6"><a href="" class="link-dark">- 宅配專區</a></li>
-      <li class="mb-6"><a href="" class="link-dark">- 禮盒專區</a></li>
-      <li class="mb-6"><a href="" class="link-dark">- 手工餅乾</a></li>
-    </ul>
-  </aside>
+  <div class="position-relative">
+    <aside class="side-menu">
+      <h3 class="fs-5 mb-6">商品類別</h3>
+      <ul class="list-group list-group-flush">
+        <li>
+          <a
+            href="#"
+            class="list-group-item list-group-item-action py-4 fs-6"
+            :class="{ active: category === '' }"
+            @click.prevent="
+              setCategory(''), this.$router.push({ name: 'products' })
+            "
+          >
+            全部商品
+          </a>
+        </li>
+        <li v-for="item in categories" :key="item">
+          <a
+            href="#"
+            class="list-group-item list-group-item-action py-4 fs-6"
+            :class="{ active: item === category }"
+            @click.prevent="
+              setCategory(item), this.$router.push({ name: 'products' })
+            "
+          >
+            {{ item }}
+          </a>
+        </li>
+      </ul>
+    </aside>
+  </div>
 </template>
 
 <script>
-export default {};
+import useProductStore from "@/stores/useProductStore";
+import { mapActions, mapState } from "pinia";
+
+export default {
+  computed: {
+    ...mapState(useProductStore, ["categories", "products", "category"]),
+
+    // filteredProducts() {
+    //   // if (this.setCategory === "all") {
+    //   //   // 如果未選擇類別，則顯示所有產品
+    //   //   return this.products;
+    //   // } else {
+    //   //   // 否則，只顯示選擇的類別相關的產品
+    //   // }
+    //   return this.products.filter((product) =>
+    //     product.category.match(this.setCategory)
+    //   );
+    // },
+  },
+  watch: {
+    // $route(to, from) {
+    //   console.log("to:", to.name, "from:", from.name);
+    //   if (to.name === "products" && from.name !== "product") {
+    //     this.setCategory("");
+    //   }
+    // },
+  },
+  methods: {
+    ...mapActions(useProductStore, ["getProducts", "setCategory"]),
+  },
+  // mounted() {
+  //   console.log(this.category);
+  // },
+  // unmounted() {
+  //   console.log("unmounted");
+  //   this.setCategory("");
+  // },
+};
 </script>
+
+<style lang="scss" scoped>
+.side-menu {
+  position: sticky;
+  top: 100px;
+}
+</style>
