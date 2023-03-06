fdb21cb44780af0a0d949af122e22b9c66736d65
KumaShow <kumadth@gmail.com>
2023-03-06 15:57:27 +0800

    更新我的最愛頁面

--- src/views/frontend/FavoriteView.vue
+++ src/views/frontend/FavoriteView.vue
@@ -1,15 +1,28 @@
 <template>
-  <section class="favorite container py-11 main-height">
-    <h2 class="text-center py-6 font-serif">我的最愛</h2>
-    <div class="text-center py-10">
+  <main class="favorite container py-11 main-height">
+    <h2 class="text-center mb-10 font-serif">我的最愛</h2>
+    <div class="text-center py-10 mb-11">
       <p class="mb-13 fs-3">目前還沒有收藏呢!</p>
       <RouterLink to="/products" class="btn btn-outline-primary">
         找甜點去
       </RouterLink>
     </div>
-  </section>
+    <!-- 推薦商品 -->
+    <section class="">
+      <h3 class="mb-8">推薦商品</h3>
+      <ProductsCarousel />
+    </section>
+  </main>
 </template>
 
-<script></script>
+<script>
+import ProductsCarousel from "@/components/frontend/ProductsCarousel.vue";
+
+export default {
+  components: {
+    ProductsCarousel,
+  },
+};
+</script>
 
 <style lang="scss" scoped></style>
