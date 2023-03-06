463995ad979606e78435f9a9a5a31d0db4d7252f
KumaShow <kumadth@gmail.com>
2023-03-06 15:55:36 +0800

    更新產品一覽頁面樣式

--- src/views/frontend/ProductsView.vue
+++ src/views/frontend/ProductsView.vue
@@ -1,18 +1,20 @@
 <template>
-  <section class="Products container py-11">
+  <main class="container py-11 main-height">
     <div class="row">
       <AsideProductMenu class="col-md-3" />
       <main class="col-md-9">
         <div class="container">
-          <h2 class="mb-9 font-serif">產品頁面</h2>
-          <ul class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-5">
+          <h2 class="mb-9 font-serif">
+            {{ category ? category : "全部商品" }}
+          </h2>
+          <ul class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-8">
             <li class="col" v-for="product in products" :key="product.id">
-              <RouterLink :to="`/product/${product.id}`">
-                <div class="card">
-                  <div class="overflow-hidden">
+              <RouterLink :to="`/product/${product.id}`" class="product-link">
+                <div class="card hover-shadow">
+                  <div class="overflow-hidden product-img">
                     <img
-                      :src="product.imageUrl"
-                      class="object-cover img-scale w-100"
+                      v-lazy="product.imageUrl"
+                      class="object-cover w-100"
                       alt="甜點"
                       style="height: 200px"
                     />
@@ -31,10 +33,16 @@
                     <h5 class="card-title fs-5 text-neutral-900 text-truncate">
                       {{ product.title }}
                     </h5>
-                    <p class="card-text text-primary fs-5 mb-6">NT$ 280</p>
-                    <a href="#" class="btn btn-outline-primary w-100"
-                      >加入購物車</a
+                    <p class="card-text text-primary fs-5 mb-6">
+                      NT$ {{ product.price }}
+                    </p>
+                    <button
+                      type="button"
+                      class="btn btn-outline-primary w-100"
+                      @click.prevent="addToCart(product.id)"
                     >
+                      加入購物車
+                    </button>
                   </div>
                 </div>
               </RouterLink>
@@ -44,35 +52,91 @@
       </main>
     </div>
     <ThePagination class="mt-10" @get-data="getProducts" :pages="pagination" />
-  </section>
+  </main>
   <RouterView />
+  <TheLoading />
 </template>
 
 <script>
 import { mapState, mapActions } from "pinia";
 import useProductStore from "@/stores/useProductStore";
 import ThePagination from "@/components/ThePagination.vue";
+import TheLoading from "@/components/TheLoading.vue";
 import AsideProductMenu from "@/components/frontend/AsideProductMenu.vue";
 
 export default {
   components: {
     ThePagination,
     AsideProductMenu,
+    TheLoading,
   },
   computed: {
-    ...mapState(useProductStore, ["products", "loadingItem", "pagination"]),
+    ...mapState(useProductStore, [
+      "products",
+      "loadingItem",
+      "pagination",
+      "category",
+    ]),
+
+    // // 根據類別篩選對應產品
+    // filteredProducts() {
+    //   if (this.category === "") {
+    //     return this.products;
+    //   } else {
+    //     return this.products.filter((product) =>
+    //       product.category.match(this.category)
+    //     );
+    //   }
+    // },
+  },
+  watch: {
+    category() {
+      this.getProducts();
+    },
   },
   methods: {
     ...mapActions(useProductStore, [
       "getProducts",
-      "getProductDetail",
+      "getProductsAll",
       "addToCart",
     ]),
   },
   mounted() {
     this.getProducts();
+    this.getProductsAll();
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.product-img {
+  position: relative;
+
+  &::after {
+    content: "查看商品";
+    color: #fff;
+    font-size: 24px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, 20%); // hover 前的位置
+    text-align: center;
+    font-size: 1.7rem;
+    opacity: 0; // hover 前不可見
+    transition: all 0.5s;
+    backface-visibility: hidden;
+  }
+}
+
+.product-link:hover {
+  img {
+    transition: all 0.3s ease;
+    transform: scale(1.2);
+    filter: brightness(60%);
+  }
+  .product-img::after {
+    opacity: 1;
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
