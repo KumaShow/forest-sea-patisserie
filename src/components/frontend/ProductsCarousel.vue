ec2faf18d89c4afc24b4ee4177c16d8cea26efa1
KumaShow <kumadth@gmail.com>
2023-03-06 15:52:33 +0800

    更新熱門商品樣式
    使用 Swiper 設置輪播效果

--- src/components/frontend/ProductsCarousel.vue
+++ src/components/frontend/ProductsCarousel.vue
@@ -2,99 +2,293 @@
   TODO: 樣式微調
 -->
 <template>
-  <ul class="row card-group">
-    <li class="col-md-3">
-      <div class="card">
-        <div class="position-relative">
-          <span
-            class="fs-8 py-1 px-2 bg-secondary text-white rounded-1 position-absolute"
-            >本月限定</span
-          >
-        </div>
-        <img src="@/assets/images/products/dessert1.png" class="" alt="甜點" />
-        <div class="card-body d-flex flex-column justify-content-between">
-          <div>
-            <h5 class="card-title fs-5 text-neutral-900 text-truncate">
-              可麗露 焦糖原味
-            </h5>
-            <div class="my-4">
-              <span class="fs-8 py-1 px-2 bg-secondary text-white rounded-1"
-                >可麗露</span
+  <swiper
+    :slidesPerView="1"
+    :spaceBetween="10"
+    :pagination="{
+      clickable: true,
+      el: '.swiper-pagination',
+    }"
+    :breakpoints="{
+      '576': {
+        slidesPerView: 1.2,
+        spaceBetween: 30,
+      },
+      '768': {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      '992': {
+        slidesPerView: 4,
+        spaceBetween: 24,
+      },
+    }"
+    :modules="modules"
+    class="mySwiper"
+  >
+    <swiper-slide>
+      <li class="col">
+        <RouterLink :to="`/product/id`" class="">
+          <div class="card box-shadow-3">
+            <div class="position-relative">
+              <span
+                class="fs-8 py-1 px-2 bg-secondary text-white rounded-1 position-absolute"
+                >本月限定</span
               >
             </div>
-            <!-- <div class="my-8 d-flex justify-content-between align-items-center"> -->
-            <p class="card-text text-primary fs-5">
-              <del class="me-2 fs-6 text-neutral-500">NT$ 70</del>NT$ 65
-            </p>
-            <!-- </div> -->
-          </div>
-          <a href="#" class="btn btn-outline-primary w-100">加入購物車</a>
-        </div>
-      </div>
-    </li>
-    <li class="col-md-3">
-      <div class="card">
-        <img src="@/assets/images/products/dessert2.png" class="" alt="甜點" />
-        <div class="card-body d-flex flex-column justify-content-between">
-          <div>
-            <h5 class="card-title fs-5 text-neutral-900 text-truncate">
-              可麗露 焦糖原味
-            </h5>
-            <div class="my-8 d-flex justify-content-between align-items-center">
-              <span class="fs-8 py-1 px-2 bg-secondary text-white rounded-1"
-                >可麗露</span
-              >
-              <p class="card-text text-primary fs-5">
-                <del class="me-2 fs-6 text-neutral-500">NT$ 70</del>NT$ 65
-              </p>
+            <div class="overflow-hidden">
+              <img
+                src="@/assets/images/products/dessert1.png"
+                class="object-cover w-100 card-img"
+                alt="甜點"
+              />
+            </div>
+            <div
+              class="card-body p-6 p-md-5 p-lg-6 d-flex flex-column justify-content-between"
+            >
+              <div>
+                <h5 class="card-title fs-5 text-neutral-900 text-truncate">
+                  可麗露 焦糖原味
+                </h5>
+                <span
+                  class="fs-8 my-4 my-md-1 py-1 px-2 bg-secondary text-white rounded-1 d-inline-block"
+                  >可麗露</span
+                >
+                <!-- <div class="my-8 d-flex justify-content-between align-items-center"> -->
+                <p class="card-text text-primary fs-5 mb-4 mb-md-2">
+                  <del class="me-2 fs-6 text-neutral-500">NT$ 70</del>NT$ 65
+                </p>
+                <!-- </div> -->
+              </div>
+              <a href="#" class="btn btn-outline-primary w-100">加入購物車</a>
             </div>
           </div>
-          <a href="#" class="btn btn-outline-primary w-100">加入購物車</a>
-        </div>
-      </div>
-    </li>
-    <li class="col-md-3">
-      <div class="card">
-        <img src="@/assets/images/products/dessert3.png" class="" alt="甜點" />
-        <div class="card-body">
-          <h5 class="card-title fs-5 text-neutral-900 text-truncate">
-            可麗露 焦糖原味
-          </h5>
-          <div class="my-3">
-            <span class="fs-8 py-1 px-2 bg-secondary text-white rounded-1"
-              >可麗露</span
-            >
-          </div>
-          <p class="card-text text-primary fs-5 mb-6">
-            <del class="me-2 fs-6 text-neutral-500">NT$ 70</del>NT$ 65
-          </p>
-          <a href="#" class="btn btn-outline-primary w-100">加入購物車</a>
-        </div>
-      </div>
-    </li>
-    <li class="col-md-3">
-      <div class="card">
-        <img src="@/assets/images/products/dessert4.png" class="" alt="甜點" />
-        <div class="card-body">
-          <div class="my-3">
-            <span class="fs-8 py-1 px-2 me-3 bg-secondary text-white rounded-1"
-              >彌月禮盒</span
-            >
-            <span class="fs-8 py-1 px-2 bg-secondary text-white rounded-1"
+        </RouterLink>
+      </li>
+    </swiper-slide>
+
+    <swiper-slide>
+      <li class="col">
+        <div class="card box-shadow-3 hover-float-z">
+          <div class="position-relative">
+            <span
+              class="fs-8 py-1 px-2 bg-secondary text-white rounded-1 position-absolute"
               >本月限定</span
             >
           </div>
-          <h5 class="card-title fs-5 text-neutral-900 text-truncate">
-            彌月禮盒 5 吋/小山圓抹茶
-          </h5>
-          <p class="card-text text-primary fs-5 mb-6">NT$ 280</p>
-          <a href="#" class="btn btn-outline-primary w-100">加入購物車</a>
+          <div class="overflow-hidden">
+            <img
+              src="@/assets/images/products/dessert2.png"
+              class="object-cover w-100 card-img"
+              alt="甜點"
+            />
+          </div>
+          <div
+            class="card-body p-6 p-md-5 p-lg-6 d-flex flex-column justify-content-between"
+          >
+            <div>
+              <h5 class="card-title fs-5 text-neutral-900 text-truncate">
+                可麗露 焦糖原味
+              </h5>
+              <span
+                class="fs-8 my-4 my-md-1 py-1 px-2 bg-secondary text-white rounded-1 d-inline-block"
+                >可麗露</span
+              >
+              <!-- <div class="my-8 d-flex justify-content-between align-items-center"> -->
+              <p class="card-text text-primary fs-5 mb-4 mb-md-2">
+                <del class="me-2 fs-6 text-neutral-500">NT$ 70</del>NT$ 65
+              </p>
+              <!-- </div> -->
+            </div>
+            <a href="#" class="btn btn-outline-primary w-100">加入購物車</a>
+          </div>
         </div>
-      </div>
-    </li>
-  </ul>
+      </li>
+    </swiper-slide>
+
+    <swiper-slide>
+      <li class="col">
+        <div class="card box-shadow-3 hover-float-z">
+          <div class="position-relative">
+            <span
+              class="fs-8 py-1 px-2 bg-secondary text-white rounded-1 position-absolute"
+              >本月限定</span
+            >
+          </div>
+          <div class="overflow-hidden">
+            <img
+              src="@/assets/images/products/dessert2.png"
+              class="object-cover w-100 card-img"
+              alt="甜點"
+            />
+          </div>
+          <div
+            class="card-body p-6 p-md-5 p-lg-6 d-flex flex-column justify-content-between"
+          >
+            <div>
+              <h5 class="card-title fs-5 text-neutral-900 text-truncate">
+                可麗露 焦糖原味
+              </h5>
+              <span
+                class="fs-8 my-4 my-md-1 py-1 px-2 bg-secondary text-white rounded-1 d-inline-block"
+                >可麗露</span
+              >
+              <!-- <div class="my-8 d-flex justify-content-between align-items-center"> -->
+              <p class="card-text text-primary fs-5 mb-4 mb-md-2">
+                <del class="me-2 fs-6 text-neutral-500">NT$ 70</del>NT$ 65
+              </p>
+              <!-- </div> -->
+            </div>
+            <a href="#" class="btn btn-outline-primary w-100">加入購物車</a>
+          </div>
+        </div>
+      </li>
+    </swiper-slide>
+
+    <swiper-slide>
+      <li class="col">
+        <div class="card box-shadow-3 hover-float-z">
+          <div class="position-relative">
+            <span
+              class="fs-8 py-1 px-2 bg-secondary text-white rounded-1 position-absolute"
+              >本月限定</span
+            >
+          </div>
+          <div class="overflow-hidden">
+            <img
+              src="@/assets/images/products/dessert3.png"
+              class="object-cover w-100 card-img"
+              alt="甜點"
+            />
+          </div>
+          <div
+            class="card-body p-6 p-md-5 p-lg-6 d-flex flex-column justify-content-between"
+          >
+            <div>
+              <h5 class="card-title fs-5 text-neutral-900 text-truncate">
+                可麗露 焦糖原味
+              </h5>
+              <span
+                class="fs-8 my-4 my-md-1 py-1 px-2 bg-secondary text-white rounded-1 d-inline-block"
+                >可麗露</span
+              >
+              <!-- <div class="my-8 d-flex justify-content-between align-items-center"> -->
+              <p class="card-text text-primary fs-5 mb-4 mb-md-2">
+                <del class="me-2 fs-6 text-neutral-500">NT$ 70</del>NT$ 65
+              </p>
+              <!-- </div> -->
+            </div>
+            <a href="#" class="btn btn-outline-primary w-100">加入購物車</a>
+          </div>
+        </div>
+      </li>
+    </swiper-slide>
+
+    <swiper-slide>
+      <li class="col">
+        <div class="card box-shadow-3 hover-float-z">
+          <div class="position-relative">
+            <span
+              class="fs-8 py-1 px-2 bg-secondary text-white rounded-1 position-absolute"
+              >本月限定</span
+            >
+          </div>
+          <div class="overflow-hidden">
+            <img
+              src="@/assets/images/products/dessert4.png"
+              class="object-cover w-100 card-img"
+              alt="甜點"
+            />
+          </div>
+          <div
+            class="card-body p-6 p-md-5 p-lg-6 d-flex flex-column justify-content-between"
+          >
+            <div>
+              <h5 class="card-title fs-5 text-neutral-900 text-truncate">
+                可麗露 焦糖原味
+              </h5>
+              <span
+                class="fs-8 my-4 my-md-1 py-1 px-2 bg-secondary text-white rounded-1 d-inline-block"
+                >可麗露</span
+              >
+              <!-- <div class="my-8 d-flex justify-content-between align-items-center"> -->
+              <p class="card-text text-primary fs-5 mb-4 mb-md-2">
+                <del class="me-2 fs-6 text-neutral-500">NT$ 70</del>NT$ 65
+              </p>
+              <!-- </div> -->
+            </div>
+            <a href="#" class="btn btn-outline-primary w-100">加入購物車</a>
+          </div>
+        </div>
+      </li>
+    </swiper-slide>
+  </swiper>
+  <div class="swiper-pagination"></div>
 </template>
 
 <script>
-export default {};
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  data() {
+    return {
+      modules: [Pagination],
+    };
+  },
+};
 </script>
+
+<style lang="scss">
+@import "bootstrap/scss/functions";
+@import "@/assets/stylesheets/helpers/variables.scss";
+@import "@/assets/stylesheets/helpers/mixin.scss";
+
+.swiper {
+  width: 100%;
+  height: 400px;
+}
+.swiper-slide {
+  .card {
+    max-height: 348px;
+  }
+
+  .card-img {
+    height: 171px;
+  }
+  @include respond-min(md) {
+    .card-img {
+      height: 150px;
+    }
+  }
+  @include respond-min(lg) {
+    .card-img {
+      height: 180px;
+    }
+  }
+}
+// 自訂分頁樣式
+.swiper-pagination-bullet {
+  height: 8px;
+  border-radius: 0;
+}
+.swiper-pagination-bullet-active {
+  width: 50px;
+  background-color: $secondary;
+  width: 16px;
+  height: 8px;
+}
+// 讓分頁移至下方
+.swiper-pagination {
+  position: static;
+  margin-top: 10px;
+}
+</style>
