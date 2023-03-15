0f80a1a18322c8c5d44d8616a6b22ca9235c334e
KumaShow <kumadth@gmail.com>
2023-03-15 23:07:00 +0800

    新增最新消息 & 文章頁面

---
+++ src/views/frontend/ArticleView.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="bg-mask-texture">
+    <main class="container py-11 main-height">
+      <div class="row justify-content-center">
+        <div class="col-9 col-md-8">
+          <article class="border-bottom mb-10">
+            <!-- 標題 -->
+            <h2 class="mb-6 font-serif">{{ article.title }}</h2>
+            <!-- 作者與時間 -->
+            <div class="d-flex align-items-center text-neutral-500 mb-8">
+              <p class="d-flex align-items-center fs-7 me-5">
+                <span class="material-symbols-outlined me-1">
+                  history_edu
+                </span>
+                作者: {{ article.author }}
+              </p>
+              <span class="me-5">|</span>
+              <p class="d-flex align-items-center fs-7">
+                <span class="material-symbols-outlined me-1">
+                  calendar_month
+                </span>
+                {{ createDate(article.create_at) }}
+              </p>
+            </div>
+            <!-- 圖片 -->
+            <figure class="mb-0 overflow-hidden">
+              <img
+                :src="article.image"
+                :alt="article.title"
+                class="img-fluid d-block object-cover w-100"
+                style="height: 350px"
+              />
+            </figure>
+            <!-- 文章內容 -->
+            <div class="my-10" v-html="article.content"></div>
+          </article>
+          <div>
+            <button
+              type="button"
+              @click="$router.go(-1)"
+              class="btn btn-primary"
+            >
+              返回
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import useArticleStore from "@/stores/useArticleStore";
+import { mapActions, mapState } from "pinia";
+
+export default {
+  components: {},
+  props: ["id"],
+  computed: {
+    ...mapState(useArticleStore, ["article"]),
+  },
+  methods: {
+    ...mapActions(useArticleStore, [
+      "getArticle",
+      "createDate",
+      "clearArticle",
+    ]),
+  },
+  mounted() {
+    this.getArticle(this.id);
+  },
+  unmounted() {
+    this.clearArticle();
+  },
+};
+</script>
--- src/views/frontend/NewsView.vue
+++ src/views/frontend/NewsView.vue
@@ -1,9 +1,105 @@
 <template>
-  <div class="News">
-    <h2 class="text-center py-6 font-serif">最新消息</h2>
+  <div class="bg-mask-texture">
+    <main class="py-11 main-height">
+      <div class="container">
+        <h2 class="text-center mb-10 font-serif">最新消息</h2>
+        <ul class="row justify-content-center">
+          <template v-for="article in articles" :key="article.id">
+            <li class="mb-8 mb-md-10 col-9 col-md-10">
+              <RouterLink
+                :to="`/article/${article.id}`"
+                class="text-dark row justify-content-center article-link border-bottom pb-8"
+              >
+                <!-- 文章圖片 -->
+                <div class="col-md-4 px-0 pe-md-4">
+                  <div class="overflow-hidden">
+                    <img
+                      v-lazy="article.image"
+                      alt="文章圖片"
+                      class="object-cover w-100"
+                      style="height: 200px"
+                    />
+                  </div>
+                </div>
+                <!-- 文章內容 -->
+                <div class="col-md-8">
+                  <div>
+                    <!-- 文章 Title -->
+                    <h3 class="fs-4 my-3 article-title">
+                      <a>{{ article.title }}</a>
+                    </h3>
+                    <!-- Tag & 時間 -->
+                    <p class="my-4 my-md-6">
+                      <template
+                        v-for="(tag, idx) in article.tag"
+                        :key="tag + idx"
+                      >
+                        <span
+                          class="fs-8 py-1 px-2 me-4 bg-secondary rounded-1 text-white"
+                        >
+                          {{ tag }}
+                        </span>
+                      </template>
+                      {{ createDate(article.create_at) }}
+                    </p>
+                    <p class="">
+                      {{ article.description }}
+                    </p>
+                  </div>
+                </div>
+              </RouterLink>
+            </li>
+          </template>
+        </ul>
+        <ThePagination :pages="pagination" />
+      </div>
+    </main>
+    <RouterView />
   </div>
 </template>
 
-<script></script>
+<script>
+import ThePagination from "@/components/ThePagination.vue";
+import useArticleStore from "@/stores/useArticleStore";
+import { mapActions, mapState } from "pinia";
 
-<style lang="scss" scoped></style>
+export default {
+  components: {
+    ThePagination,
+  },
+  computed: {
+    ...mapState(useArticleStore, [
+      "articles",
+      "pagination",
+      "filterLastArticles",
+    ]),
+  },
+  methods: {
+    ...mapActions(useArticleStore, ["getArticles", "createDate"]),
+  },
+  mounted() {
+    this.getArticles();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "bootstrap/scss/functions";
+@import "@/assets/stylesheets/helpers/variables";
+.article-link:hover {
+  h3 {
+    color: $secondary;
+  }
+  img {
+    transform: scale(1.2);
+  }
+}
+.article-link {
+  h3 {
+    transition: all 0.2s ease-out;
+  }
+  img {
+    transition: transform 0.2s ease-out;
+  }
+}
+</style>
