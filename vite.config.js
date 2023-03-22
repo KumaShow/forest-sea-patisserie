51e01cca31a9220853eca3f3622be3dc18aada0d
KumaShow <kumadth@gmail.com>
2023-03-22 17:47:12 +0800

    更新 base 路徑
    設定開發與正式環境的不同路徑

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/* global process */
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
@@ -5,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/forest-sea-patisserie/",
+  base: process.env.NODE_ENV === "production" ? "/forest-sea-patisserie/" : "",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
