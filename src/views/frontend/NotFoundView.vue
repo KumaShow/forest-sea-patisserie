6080f99c9d46e4e60dffa005a3ffd7a0af95e0da
KumaShow <kumadth@gmail.com>
2023-03-22 17:47:58 +0800

    更新404頁面樣式

--- src/views/frontend/NotFoundView.vue
+++ src/views/frontend/NotFoundView.vue
@@ -1,6 +1,29 @@
 <template>
-  <main class="">
-    <h2 class="text-center">404</h2>
-    <p>找不到網頁!</p>
+  <FrontNavBar />
+  <main
+    class="text-center min-height d-flex justify-content-center align-items-center bg-mask-texture"
+  >
+    <div>
+      <h2 class="text-center display-1 mb-8">404</h2>
+      <p class="fs-4 mb-8">對不起，您輸入的網址不存在！</p>
+      <button
+        type="button"
+        class="btn btn-secondary text-white"
+        data-bs-dismiss="offcanvas"
+        @click="this.$router.push('/products')"
+      >
+        找甜點去
+      </button>
+    </div>
   </main>
 </template>
+
+<script>
+import FrontNavBar from "@/components/frontend/FrontNavBar.vue";
+
+export default {
+  components: {
+    FrontNavBar,
+  },
+};
+</script>
